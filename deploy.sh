1454ae13029a715a82f4d01e09d3dc01559987f9
Andy Collon <andy@eligendi.com>
2019-01-03 16:22:12 +0000

    Add canary deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,10 @@ echo "Deploying pet-db..."
 kubectl apply -f kubernetes/mysql.yml --namespace default
 
 IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
-PROJECT_ID=${GCLOUD_PROJECT_ID:-devops-workshop-123}
-echo "Deploying pet-web image version: $IMAGE_VERSION"
+PROJECT_ID=${GCLOUD_PROJECT_ID:-uke12359575-devopsday}
+CURRENT_VERSION=$(kubectl get deployment pet-web --namespace default -o jsonpath="{..image}" | cut -d':' -f2)
+echo "Current version: $CURRENT_VERSION"
+echo "Deploying pet-web canary image version: $IMAGE_VERSION"
 
-cat kubernetes/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
+cat kubernetes/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$CURRENT_VERSION/" | kubectl apply -f - --namespace default
+cat kubernetes/web-canary.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
